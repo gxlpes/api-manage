62e596ddbf7572792d620d1c905b1de946f6ba4d
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-18 15:28:29 -0300

    Added controllers

---
+++ controllers/lockers.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid"; // generate a random id
+
+export const getLockers = (req, res) => {
+  res.send(lockers);
+};
+
+export const createLocker = (req, res) => {
+  const locker = req.body;
+  lockers.push({ ...locker, id: uuidv4() });
+  res.send(`Lock with the ${locker.number} added!`);
+};
+
+export const getLocker = (req, res) => {
+  const { id } = req.params;
+  const foundLocker = lockers.id((locker) => locker.id === id);
+  res.send(foundLocker);
+};
+
+export const updateLocker = (req, res) => {
+  const { id } = req.params;
+  const { number, room, state } = req.body;
+
+  const lockerToBeUpdated = lockers.find((locker) => locker.id === id);
+
+  if (number) lockerToBeUpdated.number = number;
+  if (room) lockerToBeUpdated.room = room;
+  if (state) lockerToBeUpdated.state = state;
+
+  res.send(`Locker with the ${id} has been updated!`);
+};
--- routes/lockers.js
+++ routes/lockers.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid"; // generate a random id
+import { createLocker, getLockers, getLocker, updateLocker } from "../controllers/lockers.js";
 
 const router = express.Router();
 
@@ -7,26 +7,14 @@ let lockers = [];
 
 /////////////////////// all routes are using /lockers
 // get lockers all
-router.get("/", (req, res) => {
-  res.send(lockers);
-});
+router.get("/", getLockers);
 
-// post method
-router.post("/", (req, res) => {
-  const locker = req.body;
-
-  lockers.push({ ...locker, id: uuidv4() });
-
-  res.send(`Added locker ${locker.number}`);
-});
+// post method to create locker
+router.post("/", createLocker);
 
 // get lockers id to unite
-router.get("/:id", (req, res) => {
-  //colon sign permits anything after the id that has the name of parameters
-  const { id } = req.params;
-  const foundLocker = lockers.id((locker) => locker.id === id);
-  res.send(foundLocker);
-});
+//colon sign permits anything after the id that has the name of parameters
+router.get("/:id", getLocker);
 
 // delete each locker from the array that has id
 router.delete("/:id", (req, res) => {
@@ -36,23 +24,6 @@ router.delete("/:id", (req, res) => {
 });
 
 // patch to edit locker
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const { number, room, state } = req.body;
-
-  const lockerToBeUpdated = lockers.find((locker) => locker.id === id);
-
-  if (number) {
-    lockerToBeUpdated.number = number;
-  }
-
-  if (room) {
-    lockerToBeUpdated.room = room;
-
-    if (state) {
-      lockerToBeUpdated.state = state;
-    }
-  }
-});
+router.patch("/:id", updateLocker);
 
 export default router;
